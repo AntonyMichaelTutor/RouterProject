2c8d768e3c88c6f1b0343823c0d76a17de48784c
AntonyMichaelTutor <41692717+AntonyMichaelTutor@users.noreply.github.com>
2018-09-20 21:54:33 +0530

    Inital Version

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { RouterModule } from "@angular/router";
+import { AppComponents, AppRoutes } from "./app.routing";
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    AppComponents,
+    Page3Component
+  ],
+  
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    RouterModule.forRoot(AppRoutes)
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/app/page3/page3.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
+@Component({
+  selector: 'app-page3',
+  templateUrl: './page3.component.html',
+  styleUrls: ['./page3.component.css']
+})
+export class Page3Component {
+
+  constructor(private location: Location) { }
+  goBack() {
+    this.location.back;
+  }
+  
+  
+}
